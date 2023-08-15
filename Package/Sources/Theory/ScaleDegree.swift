4c7e8ed883c99df650a1ec223b59c0b5921bb9ef
hayabusabusa <hayabusabusaSY@gmail.com>
2023-08-16 00:10:34 +0900

    feat: スケール内で利用する度数の Enum を追加

---
+++ Package/Sources/Theory/ScaleDegree.swift
@@ -0,0 +1,26 @@
+//
+//  ScaleDegree.swift
+//  
+//
+//  Created by Shunya Yamada on 2023/08/15.
+//
+
+import Foundation
+
+/// スケール内の度数を表す Enum.
+public enum ScaleDegree: Int, Equatable, CaseIterable {
+    /// 1 度.
+    case i
+    /// 2 度.
+    case ii
+    /// 3 度.
+    case iii
+    /// 4 度.
+    case iv
+    /// 5 度.
+    case v
+    /// 6 度.
+    case vi
+    /// 7 度.
+    case vii
+}
